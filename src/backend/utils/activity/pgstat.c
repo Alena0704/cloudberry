4f7cfbefee0af5cfdba9a4422b522396fd11d108
Michael Paquier <michael@paquier.xyz>
2024-09-05 14:36:57 +0900

    Fix two NULL pointer dereferences when reading custom pgstats from file
    There were two spots in pgstat_read_statsfile() where is was possible to
    finish with a null-pointer-dereference crash for custom pgstats kinds:
    - When reading stats for a fixed-numbered stats entry.
    - When reading a variable stats entry with name serialization.
    For both cases, these issues were reachable by starting a server after
    changing shared_preload_libraries so as the stats written previously
    could not be loaded.
    
    The code is changed so as the stats are ignored in this case, like the
    other code paths doing similar sanity checks.  Two WARNINGs are added to
    be able to debug these issues.  A test is added for the case of
    fixed-numbered stats with the module injection_points.
    
    Oversights in 7949d9594582, spotted while looking at a different report.
    
    Discussion: https://postgr.es/m/Ztj0Jftsn4xXuXtl@paquier.xyz
    
    Cloudberry: the injection_points test module does not exist here (it
    appeared in PostgreSQL 17), so its part of the change is left out.
    
    (cherry picked from commit 341e9a05e7b4c4fb2a1e539a5073dfd0e5b46735)

--- src/backend/utils/activity/pgstat.c
+++ src/backend/utils/activity/pgstat.c
@@ -1791,6 +1791,12 @@ pgstat_read_statsfile(void)
 					}
 
 					info = pgstat_get_kind_info(kind);
+					if (!info)
+					{
+						elog(WARNING, "could not find information of kind %u for entry of type %c",
+							 kind, t);
+						goto error;
+					}
 
 					if (!info->fixed_amount)
 					{
@@ -1871,6 +1877,12 @@ pgstat_read_statsfile(void)
 						}
 
 						kind_info = pgstat_get_kind_info(kind);
+						if (!kind_info)
+						{
+							elog(WARNING, "could not find information of kind %u for entry of type %c",
+								 kind, t);
+							goto error;
+						}
 
 						if (!kind_info->from_serialized_name)
 						{
